6e76a6c5b56adecee562df6a71300b8101252a46
juandavc12 <67342738+juandavc12@users.noreply.github.com>
2020-10-22 19:13:33 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super ();
+
+    this.state = {
+      count : 0
+    }
+    
+  };
+  incrementar (n) {
+    this.setState({count: n})
+  }
+
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.count}</span>
+        <button id="inc" onClick={() => {this.incrementar(this.state.count + 1)}}>Incrementa</button>
       </div>
     );
   }
